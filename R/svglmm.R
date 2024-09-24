d82cdf141b9984a00caa89612ad67235d4bb63b9
junghyunJJ <swordbreadjj@gmail.com>
2024-09-23 23:12:18 -0700

    need to update the output name

--- R/svglmm.R
+++ R/svglmm.R
@@ -2,31 +2,20 @@
 # 2) 'II model' allows environment-specific spatial variance and noise but assumes that th ese values are constant across every environment.
 # 3) 'III allows the spatial and noise levels to freely vary between environments.
 
-# scale_cov <- function(K) {
-#   w <- mean(diag(K))
-#   K <- K / w
-#   return(K = K, w = w)
+
+# check_K <- function(K, X0) {
+#   Xa <- X0 %*% solve(t(X0) %*% X0)
+#   KXXa <- (K %*% X0) %*% t(Xa)
+#   K <- K - KXXa - t(KXXa) + X0 %*% (t(Xa) %*% (KXXa))
+#   return(K)
 # }
-
-check_K <- function(K, X0) {
-  # Xa <- X0 %*% solve(t(X0) %*% X0)
-  # KXXa <- (K %*% X0) %*% t(Xa)
-  # K <- K - KXXa - t(KXXa) + X0 %*% (t(Xa) %*% (KXXa))
-
- 
-  # keep <- which(lower.tri(K, diag = TRUE), arr.ind = TRUE)
-  # non_mis <- rep(1, length(keep[, 1]))
-  # keep <- cbind(keep, non_mis, K[keep])
-  # keep <- keep[order(keep[, 1], keep[, 2]), ]
-  return(K)
-}
 
 fitlmm <- function(y, X, K, df, stype, etype, rescaling) {
 
   X <- cbind(1, X)
 
   cat("[", format(Sys.time()), "]", " - Rescaling 'spatial' kernel\n", sep = "")
-  K <- check_K(K, X)
+  # K <- check_K(K, X)
   ws <- mean(diag(K))
   K <- K / ws
   K_list <- list(K)
@@ -35,8 +24,7 @@ fitlmm <- function(y, X, K, df, stype, etype, rescaling) {
   if (stype == "iid") {
     cat("[", format(Sys.time()), "]", " - Rescaling 'stype = iid' kernel\n", sep = "")
     K2 <- K * tcrossprod(Z) # K * (Z Z^T)
-    K2 <- check_K(K2, X)
-
+    # K2 <- check_K(K2, X)
     w	<- mean(diag(K2))
     K2 <- K2 / w
     K_list <- append(K_list, list(K2))
@@ -46,8 +34,7 @@ fitlmm <- function(y, X, K, df, stype, etype, rescaling) {
     for (k in seq_len(df)) {
       sel_Z <- Z[, k, drop = FALSE]
       K2 <- K * tcrossprod(sel_Z) # K * (Zk Zk^T)
-      K2 <- check_K(K2, X)
-    
+      # K2 <- check_K(K2, X)
       w	<- mean(diag(K2))
       K2 <- K2 / w
       K_list <- append(K_list, list(K2))
@@ -60,8 +47,7 @@ fitlmm <- function(y, X, K, df, stype, etype, rescaling) {
     cat("[", format(Sys.time()), "]", " - Rescaling 'etype = free' kernel\n", sep = "")
     for (k in seq_len(df - 1)) {
       K3 <- diag(Z[, k]^2) # I * (Zk Zk^T)
-      K3 <- check_K(K3, X)
-    
+      # K3 <- check_K(K3, X)
       w	<- mean(diag(K3))
       K3 <- K3 / w
       K_list <- append(K_list, list(K3))
@@ -71,6 +57,11 @@ fitlmm <- function(y, X, K, df, stype, etype, rescaling) {
 
   fit <- qgg::greml(y = y, X = X, GRM = K_list)
   vc <- fit$theta
+  # browser()
+  # if (stype == 'hom') {
+  #   names(vc) <- c("v_s", "v_e")
+  # }
+  # names(vc) <- c("v_s", paste0("v_sc", seq(1, length(vc) - 2, 1)), "v_e")
   asd <- fit$asd
 
   # rescaling
@@ -82,29 +73,14 @@ fitlmm <- function(y, X, K, df, stype, etype, rescaling) {
     asd <- diag(1 / ws) %*% asd %*% diag(1 / ws)
   }
 
-  # # names of output
-  # if (model == "s") {
-  #   stype <- "hom"
-  #   etype <- "hom"
-  #   names(vc) <- c("v_s", "v_e")
-  # } else if (model == "sc") {
-  #   stype <- "iid"
-  #   etype <- "hom"
-  #   names(vc) <- c("v_s", "v_sc", "v_e")
-  # } else if (model == "sc") {
-  #   stype <- "iid"
-  #   etype <- "iid"
-  #   # names(vc) <- c("v_s", "v_sc", "v_e")
-  # } else {
-  #   warning("Please set 'modle = s' or 'modle = sc'")
-  # }
   return(list(vc = vc, asd = asd, ll = fit$ll, beta = fit$b, v_beta = fit$vb, stype = stype, etype = etype))
 }
+
+
 
 cal_cov <- function(res_fit, df) {
   sig2out <- sig2map(res_fit$vc, res_fit$stype, res_fit$etype, df)
   h2Covmat <- msm::deltamethod(sig2out$form, res_fit$vc, res_fit$asd, ses = FALSE)
-
   if (length(sig2out$h2) == 1) {
     h2 <- sig2out$h2
   } else {
@@ -113,21 +89,20 @@ cal_cov <- function(res_fit, df) {
   return(list(h2 = h2, h2Covmat = h2Covmat))
 }
 
-#TO DO
-cal_p <- function(res_h2, model) {
-  if (model == "s") {
+
+
+cal_p <- function(res_h2, stype) {
+  if (stype == "hom") {
     p <- GxEMM::Waldtest(res_h2$h2, res_h2$h2Covmat[1, 1])
-  } else if (model == "sc") {
+  } else {
     p <- sapply(seq_len(length(res_h2$h2) - 1), function(i) {
       as.numeric(GxEMM::Waldtest(res_h2$h2[i], res_h2$h2Covmat[i, i]))
     })
     p_tot <- as.numeric(GxEMM::Waldtest(res_h2$h2['tot'], sum(diag(res_h2$h2Covmat))))
     p <- c(p, p_tot)
-    names(p) <- c('s', 'sc', 'tot')
-  } else {
+    names(p) <- c('s', paste0("sc", seq(1, (length(res_h2$h2) - 2), 1)), 'tot')
   }
   names(p) <- paste0('p_', names(p))
-  
   return(p)
 }
 
@@ -148,29 +123,30 @@ svglmm <- function(y, X, K, stype = c('hom', 'iid', 'free')[1], etype = c('hom',
   vc <- res_fit$vc
   asd <- res_fit$asd
   
-
   # 2. cal cov matrix using Delta method
   cat("[", format(Sys.time()), "]", " - Calulating covariance matrix\n", sep = "")
   res_h2 <- cal_cov(res_fit, df)
   h2 <- res_h2$h2
   
-  browser()
   # 3. cal pvalue using Wald
   cat("[", format(Sys.time()), "]", " - Calulating p-value\n", sep = "")
-  p <- cal_p(res_h2, model)
-  
+  p <- cal_p(res_h2, stype)
+
+  # 4. summary resutls
   if (resfull) {
     final_res <- list(
-      vc = vc, ll = as.numeric(res_fit$ll), df = df,
-      h2 = h2, h2Covmat = res_h2$h2Covmat, h2_p = p, 
-      beta = as.numeric(res_fit$beta), v_beta = as.matrix(res_fit$v_beta)
-    ) 
+      h2 = h2, h2Covmat = res_h2$h2Covmat, h2_p = p,
+      vc = vc, asd = asd, ll = as.numeric(res_fit$ll), df = (length(vc) - 1),
+      beta = as.numeric(res_fit$beta), v_beta = diag(as.matrix(res_fit$v_beta))
+    )
   } else {
     final_res <- list(
-      vc = vc, ll = as.numeric(res_fit$ll), df = df,
-      h2 = h2, h2_p = p, 
-      beta = as.numeric(res_fit$beta), v_beta = as.matrix(res_fit$v_beta)
+      h2 = h2, h2_p = p
     )
   }
   return(final_res)
 }
+
+
+
+# summary_res
